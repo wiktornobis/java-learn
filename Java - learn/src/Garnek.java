eef58b0b16f6f29fcd5c1c2357e01ae3b147f970
Wiktor Nobis <100137990+wiktornobis@users.noreply.github.com>
2023-11-07 13:57:13 +0100

    Add files via upload

---
+++ Java - learn/src/Garnek.java
@@ -0,0 +1,16 @@
+public class Garnek {
+    final int srednica;
+    final int wysokosc;
+    String kolor;
+
+
+    public Garnek(int srednica, int wysokosc) {
+        this.srednica = srednica;
+        this.wysokosc = wysokosc;
+    }
+    public Garnek(int srednica, int wysokosc, String kolor) {
+        this.srednica = srednica;
+        this.wysokosc = wysokosc;
+        this.kolor = kolor;
+    }
+}
---
+++ Java - learn/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Garnek garnek = new Garnek(17, 15);
+        garnek.kolor = "red";
+        
+        System.out.println(garnek.srednica);
+        System.out.println(garnek.wysokosc);
+        System.out.println(garnek.kolor);
+    }
+}
