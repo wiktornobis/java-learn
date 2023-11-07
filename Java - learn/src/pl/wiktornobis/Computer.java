e509bbb8ee34cb0fa8f14db0a9dcd3580fd42cce
wiktor <nobis171@wp.pl>
2023-11-07 16:18:19 +0100

    first step project - computer

---
+++ Java - learn/src/pl/wiktornobis/Computer.java
@@ -0,0 +1,12 @@
+package pl.wiktornobis;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    private Monitor monitor;
+    private Drive drive;
+    private HeadPhones headPhones;
+
+    List<USBDevice> usbDevices = new ArrayList<>();
+}
--- Java - learn/src/pl/wiktornobis/Main.java
+++ Java - learn/src/pl/wiktornobis/Main.java
@@ -3,12 +3,5 @@ package pl.wiktornobis;
 public class Main {
 
     public static void main(String[] args) {
-        User wiktor = new User(10, "Wiktor");
-        if(wiktor.canBuyAlkohol()) {
-            System.out.println("Can buy");
-        } else {
-            System.out.println("Can not buy");
-        }
-
     }
 }
